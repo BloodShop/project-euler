cfa34d6b882f7b211058488b5500374ea72c92cd
BloodShop <koliakovcr7@gmail.com>
2023-01-07 11:15:51 +0200

    There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.

---
+++ csharp/Q009.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    public class Q009 : IEulerProblem
+    {
+        private int PERIMETER = 1000;
+
+        public string Solve()
+        {
+            for (int a = 1; a < PERIMETER; a++)
+            {
+                for (int b = a + 1; b < PERIMETER; b++)
+                {
+                    int c = PERIMETER - a - b;
+                    if (a * a + b * b == c * c)
+                    {
+                        // It is now implied that b < c, because we have a > 0
+                        return (a * b * c).ToString();
+                    }
+                }
+            }
+            throw new Exception("Not found");
+        }
+    }
+}
