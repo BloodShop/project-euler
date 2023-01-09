953c0e85e94604a9993a850590912c8cdcee2350
BloodShop <koliakovcr7@gmail.com>
2023-01-09 17:29:59 +0200

    Find the product abc

--- csharp/Q009.cs
+++ csharp/Q009.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 
 namespace csharp
 {
+    /// <summary>
+    /// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+    /// a^2 + b^2 = c^2
+    /// For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+    /// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    /// Find the product abc.
+    /// </summary>
     public class Q009 : IEulerProblem
     {
         private int PERIMETER = 1000;
---
+++ csharp/Q010.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    public class Q010 : IEulerProblem
+    {
+        /// <summary>
+        /// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+        /// 
+        /// Find the sum of all the primes below two million.
+        /// </summary>
+        /// <returns></returns>
+
+        public string Solve()
+        {
+            
+        }
+    }
+}
