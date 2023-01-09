2d2bc61166514cd0a449f2e929fed9f03da22328
BloodShop <koliakovcr7@gmail.com>
2023-01-09 20:06:35 +0200

    Find the last ten digits of the series

--- csharp/Q048.cs
+++ csharp/Q048.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Numerics;
 
 namespace csharp
 {
-    internal class Q048
+    /// <summary>
+    /// The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+    ///
+    /// Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+    /// </summary>
+    public class Q048 : IEulerProblem
     {
+        public string Solve()
+        {
+            BigInteger modulus = (BigInteger)Math.Pow(10, 10), sum = BigInteger.Zero;
+            for (int i = 1; i <= 1000; i++)
+                sum += BigInteger.ModPow(i, (BigInteger)i, modulus);
+
+            return (sum % modulus).ToString();
+        }
     }
 }
