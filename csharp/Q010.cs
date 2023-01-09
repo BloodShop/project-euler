113a44e14e794158bede43533f9dfd3ea42c5451
BloodShop <koliakovcr7@gmail.com>
2023-01-09 17:33:15 +0200

    Find the sum of all the primes below two million.

--- csharp/Q010.cs
+++ csharp/Q010.cs
@@ -7,18 +7,38 @@ using System.Threading.Tasks;
 
 namespace csharp
 {
+    /// <summary>
+    /// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+    /// 
+    /// Find the sum of all the primes below two million.
+    /// </summary>
     public class Q010 : IEulerProblem
     {
-        /// <summary>
-        /// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-        /// 
-        /// Find the sum of all the primes below two million.
-        /// </summary>
-        /// <returns></returns>
-
         public string Solve()
         {
-            
+            long num = 2000000;
+            List<long> list = new List<long>();
+            long sum = 0;
+            while (num > 2)
+            {
+                num--;
+                if (isPrime(num))
+                    list.Add(num);
+            }
+            foreach (var i in list) // O(1)
+                sum += i;
+            return sum.ToString();
+        }
+
+        private bool isPrime(long num)
+        {
+            if (num == 2)
+                return true;
+            int sqrtNum = (int)Math.Sqrt(num);
+            for (int i = sqrtNum; i >= 2; i--)
+                if (num % i == 0)
+                    return false;
+            return true;
         }
     }
 }
