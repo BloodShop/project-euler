c34985eeb6efac0347e55c8fa783ada652ab1813
Alon Kolyakov <koliakovcr7@gmail.com>
2023-02-12 19:25:03 +0200

    Update Q048.cs

--- csharp/Q048.cs
+++ csharp/Q048.cs
@@ -4,7 +4,6 @@ namespace csharp
 {
     /// <summary>
     /// The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
-    ///
     /// Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
     /// </summary>
     public class Q048 : IEulerProblem
